566b65ea96d112dd2d856e987644e1e2a640b5f9
Edoardo Rossi <edd.rossi@gmail.com>
2014-12-26 11:52:10 +0100

    Definitions Container.

--- src/main/java/org/code/definer/Main.java
+++ src/main/java/org/code/definer/Main.java
@@ -100,4 +100,34 @@ public class Main {
 			return String.format("{\"ERROR\":\"error parsing definition for %s\"}", word);
 		}
 	}
+	static class Definitions {
+		private String[] definitions;
+		private static final String STORAGE_FILE = "";
+		
+		public Definitions(String[] definitions) {
+			this.definitions = definitions;
+		}
+		
+		public String[] getDefinitions() {
+			return definitions;
+		}
+		
+		public void persistOnFile() {
+			//TODO: implementme!
+		}
+		
+		@Override
+		public String toString() {
+			return Arrays.stream(definitions)
+					.map(s -> {
+						try {
+							return new JSONObject(s).toString(2);
+						} catch (Exception e) {
+							return String.format(
+									"{\"ERROR\":\"error printing %s\"}", s);
+						}
+					})
+					.collect(Collectors.joining("\n"));
+		}
+	}
 }
